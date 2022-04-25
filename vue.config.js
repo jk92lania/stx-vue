c738889ae2fbeb2ed6259ac142785e09b135fcff
jk <jk92lania@gmail.com>
2022-04-25 12:06:10 +0900

    /stx-vue docs 생성

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath : '/stx-vue'
+  publicPath : '/stx-vue',
+  outputDir: './docs',
+
 
 })
 
